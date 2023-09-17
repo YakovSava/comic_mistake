47c29a925a9545e2ccd1e01252fff8340a697c57
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-09-17 18:10:47 +0500

    Version 0.2
    Added LOOP to main.asm

--- main.asm
+++ main.asm
@@ -4,27 +4,34 @@ entry start
 include "win32a.inc"
 
 section '.data' data readable writeable
-	caption db "Hello world!", 0
-	message db "Test message", 0
+        caption db "Hello world!", 0
+        message db "Test message", 0
 
 section '.code' code readable executable
-	start:
-		push MB_OK
-		push caption
-		push message
-		push 0
-		call [MessageBoxA]
+        start:
+                mov ecx, 5
+                mov eax, 0
 
-		push 0
-		call [ExitProcess]
+                repeat_loop:
+                        push MB_OK
+                        push caption
+                        push message
+                        push 0
+                        call [MessageBoxA]
+
+                        add eax, 2
+                        loop repeat_loop
+
+                push 0
+                call [ExitProcess]
 
 section '.idata' import data readable writeable
-	library kernel32, 'kernel32.dll',\
-		user32, 'user32.dll'
+        library kernel32, 'kernel32.dll',\
+                user32, 'user32.dll'
 
 
-	import kernel32,\
-		ExitProcess, 'ExitProcess'
+        import kernel32,\
+                ExitProcess, 'ExitProcess'
 
-	import user32,\
-		MessageBoxA, 'MessageBoxA'
+        import user32,\
+                MessageBoxA, 'MessageBoxA'
